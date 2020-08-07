715d8345b21fd31f48d1ac7c1a06529bf936480c
Kamsi Kodi <kamsikodi@gmail.com>
2020-08-07 12:49:37 +0100

    Added Banner Css

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image" src="https://png.pngtree.com/thumb_back/fw800/back_our/20190617/ourmid/pngtree-shopping-mall-poster-background-template-image_127386.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Home;
